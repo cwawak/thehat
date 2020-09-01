1d1c6a98139b4c1ab99355d63cc90f2c048ca9ae
Christopher Wawak <cwawak@redhat.com>
2020-09-01 10:22:11 -0400

    Update runme.sh

--- runme.sh
+++ runme.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
 echo "logger test"
-date
-cat /app/payload
-sleep 10
-date
-cat /app/payload
-date
-sleep 10
-date
-cat /app/payload
+
+while true
+  do date;
+  cat /app/payload;
+  sleep 10;
+  date;
+done
+  
