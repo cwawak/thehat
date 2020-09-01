b55512f8bbe653aa41adc5cc211a3323e8171069
cwawak <cwawak@cwawak.usersys.redhat.com>
2020-09-01 10:17:35 -0400

    first commit

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM fedora:latest
+
+COPY . /app
+
+CMD bash /app/runme.sh
---
+++ runme.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+echo "logger test"
+date
+cat /app/payload
+sleep 10
+date
+cat /app/payload
+date
+sleep 10
+date
+cat /app/payload
