c18cd33c128ff0a93b375d4d82294bc5de82e527
Christopher Wawak <cwawak@redhat.com>
2021-02-05 15:47:56 -0500

    Update runme.sh

--- runme.sh
+++ runme.sh
@@ -7,7 +7,7 @@ for i in {1..25}
   do date;
   message="Memory used"
   message="${message} ${i}"
-  message="${message} MB"
+  message="${message}0 MB"
   echo $message
   /usr/bin/stress-ng --vm 1 --vm-bytes 10M --timeout 600s&
   sleep 3;
